bd2eb2aa90b8b43e82494910c86fc77029236b6a
admin <baiysh.zmb@gmail.com>
2023-02-11 08:12:15 +0600

    comit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @ B_a_i_y_s_h 155f
+ * ${PROJECT_NAME}
+ * ${DATE}
+ * ${USER}
+ * ${DAY_NAME_SHORT}
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
